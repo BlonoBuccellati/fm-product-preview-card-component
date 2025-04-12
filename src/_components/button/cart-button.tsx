18a6f8b67520e207694549e7423cec2077eeeec3
BlonoBuccellati <rk.shimizu.rk@gmail.com>
2025-04-13 05:04:04 +0900

    fix(button): buttonタグの中のdivタグをspanタグに修正(divがフローコンテンツのため)

--- src/_components/button/cart-button.tsx
+++ src/_components/button/cart-button.tsx
@@ -8,10 +8,10 @@ interface CartButtonProps {
 const CartButton = ({ title, icon }: CartButtonProps) => {
   return (
     <button className="bg-Green-500 hover:bg-Green-700 active:bg-Green-700 rounded-lg py-3 font-bold text-white hover:cursor-pointer">
-      <div className="flex justify-center gap-3">
+      <span className="flex justify-center gap-3">
         <Image src={icon} alt="" width={18} className="h-[1.125rem]" />
         {title}
-      </div>
+      </span>
     </button>
   );
 };
