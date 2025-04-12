d1f5866c917126718f8f3bc56d54f2b77a5f323a
BlonoBuccellati <rk.shimizu.rk@gmail.com>
2025-04-13 04:10:01 +0900

    style(Mobile): モバイル、デスクトップ用にメディアクエリ適用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,28 @@ import HeroImage from "@/_components/hero-image";
 
 export default function Home() {
   return (
-    <div className="bg-Cream py-20text-Gray min-h-screen px-3 text-sm">
+    <div className="bg-Cream text-Gray min-h-screen px-3 py-20 text-sm md:flex">
       {/* カード */}
-      <main className="bg-White grid grid-cols-1 rounded-md md:grid-cols-2">
+      <main className="bg-White m-auto grid max-w-[600px] grid-cols-1 rounded-md md:grid-cols-2">
         {/* 画像 */}
-        <div className="relative aspect-[4/3] w-full md:aspect-[3/4]">
-          <HeroImage className="mx-auto rounded-t-md" />
+        <div className="relative aspect-[4/3] w-full md:aspect-[3/4.5]">
+          <HeroImage className="rounded-t-md md:rounded-t-none md:rounded-l-md" />
         </div>
         {/* コンテンツ */}
-        <div className="flex flex-col gap-6 p-8">
-          <div className="text-Gray tracking-[.3rem] uppercase">perfume</div>
-          <h1 className="text-preset-1 font-fraunces text-black">
-            Gabrielle Essence Eau De Parfum
-          </h1>
-          <p>
-            A floral, solar and voluptuous interpretation composed by Olivier
-            Polge, Perfumer-Creator for the House of CHANEL
-          </p>
+        <div className="flex flex-col gap-6 p-8 md:gap-8">
+          <div className="flex flex-col gap-6">
+            {/* ジャンル */}
+            <div className="text-Gray tracking-[.3rem] uppercase">perfume</div>
+            {/* 商品名 */}
+            <h1 className="text-preset-1 font-fraunces text-black">
+              Gabrielle Essence Eau De Parfum
+            </h1>
+            {/* 商品説明 */}
+            <p>
+              A floral, solar and voluptuous interpretation composed by Olivier
+              Polge, Perfumer-Creator for the House of CHANEL
+            </p>
+          </div>
           <div className="flex items-center gap-4">
             <span className="text-Green-500 font-fraunces text-preset-1">
               $149.99
